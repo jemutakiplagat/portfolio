8585af0ed7909aad0d07208573af023aa6ce2037
jemutaikiplagat <kiplagat.jemutai@gmail.com>
2022-05-16 20:22:09 +0300

    Add allthe changes on the .css .js files

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var rightNav = document.getElementById("rightNav");
+const rightNav = document.getElementById('rightNav');
+const navLinks = document.getElementById('menu');
+function showMenu() {
+  rightNav.style.right = '0';
+ 
+}
+function hideMenu() {
+  rightNav.style.right = '-500px';
+  rightNav.classList.remove('active');
+}
+rightNav.forEach((n) => n.addEventListener('click', hideMenu));
 
-function showMenu(){
- rightNav.style.right = "0";
+function afterClick() {
+  rightNav.classList.remove('active');
 }
-function hideMenu(){
- rightNav.style.right = "-500px";
-}
+
+navLinks.forEach((n) => n.addEventListener('click', afterClick));
+rightNav.forEach((n) => n.addEventListener('click', showMenu));
