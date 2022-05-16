59254346863975308f38fa7cd98518e7204ca934
jemutaikiplagat <kiplagat.jemutai@gmail.com>
2022-05-16 20:53:51 +0300

    Add the changes on the .js file

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const rightNav = document.getElementById('rightNav');
 const navLinks = document.getElementById('menu');
 function showMenu() {
   rightNav.style.right = '0';
- 
 }
 function hideMenu() {
   rightNav.style.right = '-500px';
